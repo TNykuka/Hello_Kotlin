0660537c83ae401aae00847346c54a5a9c5e6c7a
TNy-Kk <kingkuka088@gmail.com>
2020-07-13 00:10:00 +0700

    Lesson4_7

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -15,13 +15,13 @@ class Aquarium(val width: Int = 0,var height: Int = 40,val length: Int = 100){
 
 
 
-class Aquarium(var length: Int = 100,var width: Int =20,var height: Int = 40){
+open class Aquarium(var length: Int = 100,var width: Int =20,var height: Int = 40){
 
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
         set(value){height = (value * 1000) / (width * length) }
 
-    var water = volume * 0.9
+    open var water = volume * 0.9
 
     constructor(numberOfFish: Int): this(){
 
@@ -29,4 +29,13 @@ class Aquarium(var length: Int = 100,var width: Int =20,var height: Int = 40){
         val tank: Double = water + water * 0.1
         height = (tank / (length * width)).toInt()
     }
+}
+class TowerTank(): Aquarium(){
+    override  var water = volume *0.8
+
+    override var volume: Int
+        get() = (width * height * length / 1000 * PI).toInt()
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 }
