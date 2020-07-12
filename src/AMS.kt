b3b58f2a41ded1315879eb4036002f66292ba303
TNy-Kk <kingkuka088@gmail.com>
2020-07-12 19:40:07 +0700

    Lesson3_16

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,19 +1,84 @@
-fun main(args: Array<String>) {
-    var fortune: String = ""
-    while (!fortune.contains("Take it easy")) {
-        fortune = getFortune(getBirthday())
-        println("\nYour fortune is: $fortune")
+import java.util.*
+
+fun main(args: Array<String>){
+    println("Hello, ${args[0]}!")
+    feedTheFish()
+    eagerExample()
+
+    var bubbles = 0
+    while (bubbles < 50){
+        bubbles++
+    }
+
+    //repeat (function from standard library)
+    repeat(2){
+        println("A fish is swimming")
     }
 }
-fun isVeryHot (temperature: Int) = temperature > 35
-fun isSadRainyCold (mood: String, weather: String, temperature: Int) =
-    mood == "sad" && weather == "rainy" && temperature == 0
-fun isHappySunny (mood: String, weather: String) = mood == "happy" && weather == "sunny"
-fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24) : String {
-    return when {
-        isVeryHot(temperature) -> "go swimming"
-        isSadRainyCold(mood, weather, temperature) -> "stay in bed"
-        isHappySunny(mood, weather) -> "go for a walk"
-        else -> "Stay home and read."
+
+fun shouldChangeWater(
+    day: String,
+    temperature: Int = 22,
+    dirty: Int = 20) : Boolean {
+    val isTooHot = temperature > 30
+    val isDirty = dirty > 30
+    val isSunday = day == "Sundey"
+    return when{
+        isTooHot(temperature)-> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+
+    }
+}
+
+fun isTooHot(temperature: Int) = temperature >30
+fun isDirty(dirty: Int) = dirty > 30
+fun  isSunday(day: String) = day == "Sunday"
+
+fun eagerExample(){
+    val decoration = listOf ("rock","pagoda","plastic plant","alligator","flowerpot")
+
+    val eager = decoration.filter{it[0] == 'p'}
+    println(eager)
+
+    // apply filter lazily
+    val filtered = decoration.asSequence().filter { it[0] == 'p' }
+    println(filtered)
+    println(filtered.toList())
+
+    //apply map lazily
+    val lazyMap = decoration.asSequence().map {
+        println("map: $it")
+        it
+    }
+    println(lazyMap)
+    println("first: ${lazyMap.first()}")
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+
+    if (shouldChangeWater(day)){
+        println("Change the water today")
+    }
+}
+
+
+fun  randomDay() : String{
+    val week = listOf ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+    return week[Random().nextInt(7)]
+}
+fun fishFood (day : String) : String{
+    var food = "fasting"
+    return when (day){
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "fasting"
     }
 }
