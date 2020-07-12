c7fb561329e25d8d9535373733f6ab6b8ec25eec
TNy-Kk <kingkuka088@gmail.com>
2020-07-12 16:06:00 +0700

    Lesson3_7.2

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
-    println("\nYour fortune is: ${getFortuneCookie()}")
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortuneCookie()
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
 }
 
 fun getFortuneCookie() : String {
