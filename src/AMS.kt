83eaa25127b5f78fb176ea66513469418273880b
TNy-Kk <kingkuka088@gmail.com>
2020-07-12 16:05:38 +0700

    Lesson3_7.1

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,15 +1,16 @@
-import java.util.*
+fun main(args: Array<String>) {
+    println("\nYour fortune is: ${getFortuneCookie()}")
+}
 
-fun main(args: Array<String>){
-    println("Hello, ${args[0]}!")
-    feedTheFish()
-}
-fun feedTheFish(){
-    val day = randomDay()
-    val food = "Pellets"
-    println("Today is $day and the fish eat $food")
-}
-fun  randomDay() : String{
-    val week = listOf ("Monday","Tuesday","Wednesday","thursday","Friday","Saturday","Sunday")
-    return week[Random().nextInt(7)]
+fun getFortuneCookie() : String {
+    val fortunes = listOf( "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends, because they are your greatest fortune.")
+    print("\nEnter your birthday: ")
+    val birthday = readLine()?.toIntOrNull() ?: 1
+    return fortunes[birthday.rem(fortunes.size)]
 }
