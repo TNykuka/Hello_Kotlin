dc5f2aa9f20eb8c8dbe65d3172b0992829007539
TNy-Kk <kingkuka088@gmail.com>
2020-07-12 16:03:47 +0700

    Lesson3_6

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,13 +1,15 @@
+import java.util.*
+
 fun main(args: Array<String>){
     println("Hello, ${args[0]}!")
-
-    val isUnit = println("this is an expression")
-    println(isUnit)
-
-    val temperature = 10
-    val isHot = if (temperature > 50 ) true else false
-    println(isHot)
-
-    val message = "You are ${ if (temperature > 50 ) "fried" else "safe" } fish"
-    println(message)
+    feedTheFish()
+}
+fun feedTheFish(){
+    val day = randomDay()
+    val food = "Pellets"
+    println("Today is $day and the fish eat $food")
+}
+fun  randomDay() : String{
+    val week = listOf ("Monday","Tuesday","Wednesday","thursday","Friday","Saturday","Sunday")
+    return week[Random().nextInt(7)]
 }
