8083f7701b34734d5a3aa1e81de3dde8c2bcf7a7
TNy-Kk <kingkuka088@gmail.com>
2020-07-13 23:36:17 +0700

    Lesson5_6

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -2,7 +2,7 @@ package Aquarium
 
 import java.lang.Math.PI
 
-/*fun main(args: Array<String>){
+fun main(args: Array<String>){
     val myAquarium: Aquarium = buildAquarium()
     println("volume: ${myAquarium.volume}")
 }
@@ -13,11 +13,11 @@ class Aquarium(val width: Int = 0,var height: Int = 40,val length: Int = 100){
     var volume
         get() = width * height * length / 1_000
         set(value) {height = (value * 1000) / (width * length)}
-}*/
+}
 
 
 
-open class Aquarium(var length: Int = 100,var width: Int =20,var height: Int = 40){
+/*open class Aquarium(var length: Int = 100,var width: Int =20,var height: Int = 40){
 
     open var volume: Int
         get() = width * height * length / 1000
@@ -41,3 +41,4 @@ class TowerTank(): Aquarium(){
             height = (value * 1000) / (width * length)
         }
 }
+*/
