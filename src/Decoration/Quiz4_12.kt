63207171324bab3048b09a82ce2495d947054182
TNy-Kk <kingkuka088@gmail.com>
2020-07-13 21:52:40 +0700

    Lesson4_13

--- src/Decoration/Quiz4_12.kt
+++ src/Decoration/Quiz4_12.kt
@@ -1,10 +1,2 @@
 package Decoration
 
-data class SpiceContainer(var spice: Spice) {
-    val label = spice.name
-}
-val spiceCabinet = listOf(SpiceContainer(Curry("Yellow Curry", "mild")),
-    SpiceContainer(Curry("Red Curry", "medium")),
-    SpiceContainer(Curry("Green Curry", "spicy")))
-
-for(element in spiceCabinet) println(element.label)
