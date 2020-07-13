bb8f6caf997efd2ceee07ece259993f3ee95b884
TNy-Kk <kingkuka088@gmail.com>
2020-07-13 23:44:17 +0700

    Lesson5_7

--- src/Aquarium/Quiz.kt
+++ src/Aquarium/Quiz.kt
@@ -1,8 +1,16 @@
-val allBooks = setOf("Macbeth", "Romeo and Juliet", "Hamlet", "A Midsummer Night's Dream")
-val library = mapOf("Shakespeare" to allBooks)
-println(library.any { it.value.contains("Hamlet") })
+const val MAX_NUMBER_BOOKS = 20
 
-val moreBooks = mutableMapOf<String, String>("Wilhelm Tell" to "Schiller")
-moreBooks.getOrPut("Jungle Book") { "Kipling" }
-moreBooks.getOrPut("Hamlet") { "Shakespeare" }
-println(moreBooks)
+fun canBorrow(hasBooks: Int): Boolean {
+    return (hasBooks < MAX_NUMBER_BOOKS)
+}
+
+object Constants {
+    const val BASE_URL = "http://www.turtlecare.net/"
+}
+
+fun printUrl() {
+    println(Constants.BASE_URL + title + ".html")
+}
+companion object {
+    val BASE_URL = "http://www.turtlecare.net/"
+}
